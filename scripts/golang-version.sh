1e12c4c285b2298c202450d0969b57dc758fc74a
Hussein Galal <galal-hussein@users.noreply.github.com>
2022-08-29 22:43:31 +0200

    Remove goboring traces from build script (#31)
    * Remove goboring traces
    
    * more fixes
    
    * only use jq

--- scripts/golang-version.sh
+++ scripts/golang-version.sh
@@ -4,12 +4,15 @@ set -x
 
 cd $(dirname $0)
 
+BUILD_BASE_RELEASE_URL=https://api.github.com/repos/rancher/image-build-base/releases
+BUILD_BASE_REQ_HEADERS="Accept: application/vnd.github+json"
 which yq > /dev/null || go install github.com/mikefarah/yq/v4@v4.23.1
+which jq > /dev/null || apk add -q --no-progress jq
 
 K8S_VERSION=$(./semver-parse.sh $1 all)
-DEPENDENCIES_URL="https://raw.githubusercontent.com/kubernetes/kubernetes/${K8S_VERSION}/build/dependencies.yaml"
-GOBORING_RELEASES_URL="https://raw.githubusercontent.com/golang/go/dev.boringcrypto/misc/boring/RELEASES"
-GOLANG_VERSION=$(curl -sL "${DEPENDENCIES_URL}" | yq e '.dependencies[] | select(.name == "golang: upstream version").version' -)
-GOBORING_VERSION=$(curl -sL  "${GOBORING_RELEASES_URL}" | awk "/${GOLANG_VERSION}b.+ [0-9a-f]+ src / {sub(/^go/, \"v\", \$1); print \$1}")
 
-echo ${GOBORING_VERSION}
+DEPENDENCIES_URL="https://raw.githubusercontent.com/kubernetes/kubernetes/${K8S_VERSION}/build/dependencies.yaml"
+GOLANG_VERSION=$(curl -sL "${DEPENDENCIES_URL}" | yq e '.dependencies[] | select(.name == "golang: upstream version").version' -)
+
+GOBORING_TAG=$(curl -s -H ${BUILD_BASE_REQ_HEADERS} ${BUILD_BASE_RELEASE_URL} | jq -r '[ .[] | select(.tag_name|contains("'${GOLANG_VERSION}'")) | .tag_name ] | sort | last')
+echo ${GOBORING_TAG}
