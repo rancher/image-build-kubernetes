5ba822f50c33ff9cea116c13851ba70b1d3bfeaf
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2022-08-29 21:40:55 +0200

    more fixes

--- scripts/golang-version.sh
+++ scripts/golang-version.sh
@@ -7,13 +7,12 @@ cd $(dirname $0)
 BUILD_BASE_RELEASE_URL=https://api.github.com/repos/rancher/image-build-base/releases
 BUILD_BASE_REQ_HEADERS="Accept: application/vnd.github+json"
 which yq > /dev/null || go install github.com/mikefarah/yq/v4@v4.23.1
-
-which jq > /dev/null || curl -Ss -o /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x /usr/bin/jq
+which jq > /dev/null || apk add -q --no-progress jq
 
 K8S_VERSION=$(./semver-parse.sh $1 all)
 
 DEPENDENCIES_URL="https://raw.githubusercontent.com/kubernetes/kubernetes/${K8S_VERSION}/build/dependencies.yaml"
 GOLANG_VERSION=$(curl -sL "${DEPENDENCIES_URL}" | yq e '.dependencies[] | select(.name == "golang: upstream version").version' -)
 
-GOBORING_TAG=$(curl -s -H ${BUILD_BASE_REQ_HEADERS} ${BUILD_BASE_RELEASE_URL} | jq '.[] | select(.tag_name| contains("'"${GOLANG_VERSION}"'"))' | jq -r .tag_name | head -1)
+GOBORING_TAG=$(curl -s -H ${BUILD_BASE_REQ_HEADERS} ${BUILD_BASE_RELEASE_URL} | jq -r '[ .[] | select(.tag_name| contains("'"${GOLANG_VERSION}"'")) | .tag_name ] | sort | last'
 echo ${GOBORING_TAG}
