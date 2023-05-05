7339ac540f3211859925a8360f29e78d777b0fb3
Brian Downs <brian.downs@gmail.com>
2023-05-05 16:58:43 -0700

    update base image
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- scripts/golang-version.sh
+++ scripts/golang-version.sh
@@ -14,7 +14,7 @@ GOLANG_MINOR=$(echo $GOLANG_VERSION | awk -F. '{print $2}')
 
 # goboring is built into Go as of 1.19; tag is 'b1' for all releases
 if [ "$GOLANG_MINOR" -ge "19" ]; then
-    GOBORING_VERSION="v${GOLANG_VERSION}b1"
+    GOBORING_VERSION="v${GOLANG_VERSION}b2"
 else
     GOBORING_VERSION=$(curl -sL  "${GOBORING_RELEASES_URL}" | awk "/${GOLANG_VERSION}b.+ [0-9a-f]+ src / {sub(/^go/, \"v\", \$1); print \$1}")
 fi
